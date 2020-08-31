97cb081d6414d06c72dd0bb9d6c9bd2a09de4d94
GitHub Action <action@github.com>
2020-08-31 17:08:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/antlersoft/bbqforidea/MyBundle.kt
+++ src/main/kotlin/com/github/antlersoft/bbqforidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.antlersoft.bbqforidea
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/antlersoft/bbqforidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/antlersoft/bbqforidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.antlersoft.bbqforidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.antlersoft.bbqforidea.MyBundle
 
 class MyApplicationService {
 
